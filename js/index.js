5a1a9fd8d52ae8cd7ea4c00af776097a7368796f
sterarpen <2482723192@qq.com>
2024-04-19 12:26:32 +0800

    update music playback function

--- js/index.js
+++ js/index.js
@@ -2,6 +2,17 @@ window.onload = function() {
     fetch("/data/music.json").then(response => response.json()).then(data => {
         const bgm = document.getElementById("bgm");
         bgm.setAttribute("src", data[0]);
-        bgm.play();
+        bgm.setAttribute("autoplay", true);
+
+        document.onclick = function() {
+            document.querySelector("audio").play();
+            const message = document.getElementById("message");
+            message.style.display = "none";
+        };
     });
+
+    // setTimeout(function() {
+    //     const message = document.getElementById("message");
+    //     message.style.display = "none";
+    // }, 5000);
 }
